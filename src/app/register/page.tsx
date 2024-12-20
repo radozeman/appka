465d471af00b66a2bb7fd568108c9fb64a5309aa
radozeman <r.zeman96@gmail.com>
2024-12-20 23:09:36 +0100

    register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,11 @@
-const page = () => {
-  return <div>page</div>;
-};
+import { RegisterForm } from "@/features/register/register-form";
 
-export default page;
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <RegisterForm />
+      </div>
+    </div>
+  );
+}
