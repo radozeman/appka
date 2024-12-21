821abfa26c70c2b9b31ec44f881e1cc336d2b577
radozeman <r.zeman96@gmail.com>
2024-12-21 10:50:04 +0100

    fix: tw cfg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,11 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./@/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
@@ -58,6 +60,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  // plugins: [require("tailwindcss-animate")],
 };
 export default config;
