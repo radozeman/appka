cffc00626a7b3036103f11822c8c7e9ca8a98c18
radozeman <r.zeman96@gmail.com>
2024-12-25 21:17:16 +0100

    dashboard no data

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,10 +1,16 @@
 import { getAuth } from "@/auth/cookie";
+import NoData from "@/features/dashboard/components/no-data";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
   const { session } = await getAuth();
   if (!session) redirect("/login");
-  return <div>page</div>;
+  const data = true;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      {data && <NoData />}
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/features/dashboard/components/no-data.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { CaptionsOff, PlusIcon } from "lucide-react";
+import Link from "next/link";
+
+const NoData = () => {
+  return (
+    <div className="text-center max-w-72">
+      <CaptionsOff className="text-muted-foreground stroke-[0.5px] size-32 mb-2.5 mx-auto" />
+      <p className="text-muted-foreground mb-6">
+        Zdá sa, že zatiaľ nemáte žiadne zaznamenané údaje.
+      </p>
+      <Button asChild>
+        <Link href="/create">
+          <PlusIcon />
+          Vytvoriť záznam
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default NoData;
