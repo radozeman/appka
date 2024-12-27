f1080fdbf38ca2d2899679cad5421c737b6e63aa
radozeman <r.zeman96@gmail.com>
2024-12-27 15:04:58 +0100

    fix: pwa path

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import withSerwistInit from "@serwist/next";
 /** @type {import('next').NextConfig} */
 
 const withSerwist = withSerwistInit({
-  swSrc: "app/sw.ts",
+  swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
   cacheOnNavigation: true,
 });
